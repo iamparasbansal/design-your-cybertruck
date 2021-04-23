aea4d861d4338dc9f2bb84e3caa252c861cbc37b
Paras Bansal <parasbansal685@gmail.com>
2021-04-24 04:04:09 +0530

    But Now form responsive

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ function togglePopup2(){
   document.getElementById("popup-2").classList.toggle("active2");
 }
 
+function togglePopup3(){
+  document.getElementById("popup-3").classList.toggle("active3");
+}
+
 function windowsalert(){
-  window.alert("Sorry! This Button was still under construction...!!!");
+  window.alert("Hi! Can't accept orders right now :(")
 }
